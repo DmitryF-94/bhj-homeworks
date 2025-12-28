1adffe85ed55f8d96eb63ca4e13fe03263ade055
Dmitry <dima.fatkhullin.94@gmail.com>
2025-12-28 20:44:38 +0300

    first commit mole-game

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,38 @@
+let score = 0;
+let misses = 0;
+let winScore = 10;
+let loseScore = 5;
+let holes = document.querySelectorAll('.hole');
+let dead = document.getElementById('dead');
+let lost = document.getElementById('lost');
+
+function resetGame() {
+	score = 0;
+	misses = 0;
+	dead.textContent = score;
+	lost.textContent = misses;
+}
+
+getHole = index => document.getElementById(`hole${index}`);
+
+holes.forEach(hole => {
+	hole.addEventListener('click', () => {
+		if (hole.classList.contains('hole_has-mole')) {
+			score++;
+			dead.textContent = score;
+
+			if (score === winScore) {
+				alert('Вы выиграли!');
+				resetGame();
+			}
+		} else {
+			misses++;
+			lost.textContent = misses;
+
+			if (misses === loseScore) {
+				alert('Вы проиграли!');
+				resetGame();
+			}
+		}
+	});
+});
