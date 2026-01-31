0d01cada59a9c45b85f5a9ecb837b9f752b8a4e9
Dmitry <dima.fatkhullin.94@gmail.com>
2026-01-31 12:14:07 +0300

    first commit tabs

---
+++ event-object/tabs/task.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+	let tabs = document.querySelectorAll('.tab');
+	let contents = document.querySelectorAll('.tab__content');
+
+	function switchTab(index) {
+		tabs.forEach((tab) => tab.classList.remove('tab_active'));
+		contents.forEach((content) => content.classList.remove('tab__content_active'));
+		tabs[index].classList.add('tab_active');
+		contents[index].classList.add('tab__content_active');
+	}
+
+	tabs.forEach((tab, index) => {
+		tab.addEventListener('click', () => switchTab(index));
+	});
+});
