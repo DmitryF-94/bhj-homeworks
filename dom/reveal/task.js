ef5d2226378d5ae34b8689125012028e015b4a10
Dmitry <dima.fatkhullin.94@gmail.com>
2026-01-31 20:29:24 +0300

    %comment%

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,14 +1,14 @@
- function checkVisibility() {
- 	let reveals = document.querySelectorAll('.reveal');
- 	let windowHeight = window.innerHeight;
- 	reveals.forEach(reveal => {
- 		let rect = reveal.getBoundingClientRect();
- 		if (rect.top < windowHeight && rect.bottom > 0) {
- 			reveal.classList.add('reveal_active');
- 		} else {
- 			reveal.classList.remove('reveal_active');
- 		}
- 	});
- }
+function checkVisibility() {
+	let reveals = document.querySelectorAll('.reveal');
+	let windowHeight = window.innerHeight;
+	reveals.forEach(reveal => {
+		let rect = reveal.getBoundingClientRect();
+		if (rect.top < windowHeight && rect.bottom > 0) {
+			reveal.classList.add('reveal_active');
+		} else {
+			reveal.classList.remove('reveal_active');
+		}
+	});
+}
 
- window.addEventListener('scroll', checkVisibility);
+window.addEventListener('scroll', checkVisibility);
